e8bbbeba4ce15b102921a2c811a54cc4647f399b
Nicola Hastings <nhastings@higherlogic.com>
2023-08-03 16:31:51 -0400

    NH | adding white background for search | resetting .bodyBackground colour

--- thrive/kids-gardening-thrive-general.js
+++ thrive/kids-gardening-thrive-general.js
@@ -57,6 +57,67 @@ function handleEvents() {
     });
 }
 
+function handleFundingOpportunities() {
+    $('.funding-opportunities ul li').each(function () {
+        // Grab href for each news item
+        var self = $(this),
+            title = $(self).find("h3"),
+            href = $(self)
+                .find("h3 a")
+                .attr("href");
+
+        // handle image
+        var imgContainer = '<div class="img-container loading" />';
+        // $(self).wrapInner('<div class="text-container" />');
+        $(self).prepend(imgContainer);
+        //   Ajax Call for each news item
+        $.ajax({
+            url: href,
+            dataType: "html",
+            success: success
+        });
+
+
+        function success(resp) {
+            //   Get Image for each news item
+
+            if ($(resp).find('.blogs-block').length) {
+                var img = $(resp).find('.blogs-block .col-md-12 img');
+                var deadline = $(resp).find('.blogs-block .col-md-12 h5');
+            }
+            if ($(resp).find('div[id*="DetailPanel"]').length) {
+                var img = $(resp).find(
+                    'div[id*="DetailPanel"] .row:nth-child(2) .col-md-10 .col-md-12 img:first-of-type'
+                );
+            }
+
+            if ($(img).attr('src') === undefined) {
+                $(self).find(".img-container").addClass("no-image");
+            }
+
+            var src = $(img).attr("src"),
+                //   Format for background inline style
+                url = "url('" + src + "')";
+         
+            // Set each news image into respective img-containers
+            $(self)
+                .find(".img-container")
+                .css("background-image", url);
+            //   2 second timeout added to allow for ajax to load img
+            setTimeout(function () {
+                $(self)
+                    .find(".img-container")
+                    .removeClass("loading");
+            }, 2000);
+
+            $(self).find('.title-row').append($(deadline));
+
+        }
+
+    });
+}
+
 $(function () {
     handleEvents();
+    handleFundingOpportunities();
 });
