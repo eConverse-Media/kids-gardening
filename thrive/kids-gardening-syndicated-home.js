f407a3adabbbd1f73a297389affdf6feaa28bd4c
Nicola Hastings <nhastings@higherlogic.com>
2023-08-10 16:00:36 -0400

    NH | sponsors

--- thrive/kids-gardening-syndicated-home.js
+++ thrive/kids-gardening-syndicated-home.js
@@ -2,6 +2,44 @@ function handleGroups() {
     $('.groups-btn').insertAfter('.popular-groups .pagination');
 }
 
+function handleBottomSlider() {
+    // handle slide images
+    $('.bottom-slide').each(function () {
+        var self = $(this),
+            img = $(self).find('img'),
+            htmlContent = $(self).find('.HtmlContent');
+
+        $(img).insertBefore(htmlContent);
+        $(img).wrap('<div class="img-container" />');
+
+        $(self).find('p:empty').remove();
+    });
+
+    // create carousel
+    $('.bottom-carousel > .col-md-12').slick({
+        dots: false,
+        arrows: true,
+        prevArrow: '<button type="button" class="slick-arrow prev-arrow"><i class="fa-light fa-arrow-left"></i></button>',
+        nextArrow: '<button type="button" class="slick-arrow next-arrow"><i class="fa-light fa-arrow-right"></i></button>'
+    });
+
+}
+
+function handlePartnerCarousel() {
+    $('.partners > .col-md-12').slick({
+        dots: false,
+        arrows: true,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        centerMode: true,
+        centerPadding: 'calc(50% - 615px)',
+        prevArrow: '<button type="button" class="slick-arrow prev-arrow"><i class="fa-light fa-arrow-left"></i></button>',
+        nextArrow: '<button type="button" class="slick-arrow next-arrow"><i class="fa-light fa-arrow-right"></i></button>'
+    })
+}
+
 $(function () {
     handleGroups();
+    handleBottomSlider();
+    handlePartnerCarousel();
 });
